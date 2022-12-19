6182c48073b8b6fd260c0364bbfaa22a390fb671
Rudy Menjivar <rudy.menjivar@gmail.com>
2022-12-18 16:10:24 -0800

    organized code and dynamic data separately

---
+++ data.js
@@ -0,0 +1,52 @@
+export const creatorDetails = {
+    accountHandle: '@thebundalorian',
+    creatorName: ' - the bundalorian',
+    tagline: 'this is the (binky) way..',
+    profilePic: './assets/profile-pic.jpg',
+  }
+  
+  export const socialLinks = [
+    {
+      url: 'tiktok.com/@thebundalorian',
+      iconName: 'tiktok',
+      active: true,
+    },
+    {
+      url: 'http://www.youtube.com/@bundalorian',
+      iconName: 'youtube',
+      active: true,
+    },
+    {
+      url: '',
+      iconName: 'twitter',
+      active: false,
+    },
+    {
+      url: '',
+      iconName: 'facebook',
+      active: false,
+    },
+    {
+      url: 'https://www.instagram.com/thebundalorian/',
+      iconName: 'instagram',
+      active: true,
+    },
+  ];
+  
+  export const whereNextLinks = [
+    {
+      url: '',
+      text: 'See my zoomies',
+      active: true,
+    },
+    {
+      url: '',
+      text: 'Watch me binky',
+      active: true,
+    },
+    {
+      url: '',
+      text: 'Nom Nom is the way',
+      active: true,
+    },
+  ]
